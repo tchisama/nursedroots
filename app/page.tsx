ef902f4e027b6484879e493f0ea90d32e321f0b1
tchisama <pro.tchisama@gmail.com>
2024-12-15 20:30:46 +0000

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -163,7 +163,7 @@ export default function BeautySalon() {
               <div className="space-y-2">
                 <p className="text-sm text-gray-600">
                   Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
+                  typesetting industry. Lorem Ipsum has been the industrys
                   standard dummy text ever since the 1500s, when an unknown
                   printer took a galley of type and scrambled it to make a type
                   specimen book.
